bf1443b1296a9e3a22ade58190279ecc5252be20
Denis Denisenko <denis.denisenko@gmail.com>
2024-08-30 12:51:06 +0300

    finish sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,13 @@
+"use client";
 import Link from "next/link";
 import Image from "next/image";
 import React from "react";
 import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
 
 const Sidebar = () => {
+  const pathName = usePathname();
   return (
     <section className="sidebar">
       <nav className="flex flex-col gap-4">
@@ -23,8 +27,14 @@ const Sidebar = () => {
           <h1 className="sidebar-logo">Stockify</h1>
         </Link>
         {sidebarLinks.map((item) => {
+          const isActive =
+            pathName === item.route || pathName.startsWith(`${item.route}/`);
           return (
-            <Link href={item.route} key={item.label}>
+            <Link
+              href={item.route}
+              key={item.label}
+              className={cn("sidebar-link", { "bg-bank-gradient": isActive })}
+            >
               {item.label}
             </Link>
           );
