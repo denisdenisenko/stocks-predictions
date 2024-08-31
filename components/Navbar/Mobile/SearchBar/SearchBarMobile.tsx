b218981b353d37ef9b11457c83d1ff9ca10e29e0
Nadia <nadiahosisy@gmail.com>
2024-08-31 23:03:52 +0300

    fix

--- components/Navbar/Mobile/SearchBar/SearchBarMobile.tsx
+++ components/Navbar/Mobile/SearchBar/SearchBarMobile.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 const SearchBarMobile = () => {
   return (
     <div
-      className="flex items-center h-[40px] w-[180px] rounded-[10px] border border-purpleHeart-50 px-[10px] py-[6px] gap-[8px]"
+      className="flex items-center h-[40px] w-[200px] rounded-[10px] border border-purpleHeart-50 px-[10px] py-[6px] gap-[8px]"
       style={{ backgroundColor: "rgba(243, 241, 255, 0.6)" }}
     >
       <div className="flex items-center flex-row gap-[10px] w-[141px] h-[20px]">
