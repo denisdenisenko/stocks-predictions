6349a5ef1057745fe2d83c875dc3aefaa9b3b2e7
Denis Denisenko <denis.denisenko@gmail.com>
2024-09-03 19:18:17 +0300

    add architecture for dashboard

--- app/(root)/my-dashboard/page.tsx
+++ app/(root)/my-dashboard/page.tsx
@@ -1,7 +1,18 @@
+import DesktopDashboard from "@/components/Dashboard/Desktop/DesktopDashboard";
+import MobileDashboard from "@/components/Dashboard/Mobile/MobileDashboard";
 import React from "react";
 
 const MyDashboard = () => {
-  return <div>MyDashboard</div>;
+  return (
+    <div>
+      <div className="hidden md:block p-[6px_0px_10px_0px]">
+        <DesktopDashboard />
+      </div>
+      <div className="md:hidden">
+        <MobileDashboard />
+      </div>
+    </div>
+  );
 };
 
 export default MyDashboard;
